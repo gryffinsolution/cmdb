f8e21bc2eb4789b942eadde17366a8245e5674dc
gryffinsolution <gryffinsolution@gmail.com>
2018-09-18 21:02:29 +0900

    t

--- src/main/CMDBMgr.java
+++ src/main/CMDBMgr.java
@@ -26,7 +26,7 @@ public class CMDBMgr {
 			LOG.error("  Message:    " + e.getMessage());
 			e = e.getNextException();
 		}
-	}
+	} 
 
 	public static void main(String[] args) {
 		Conf cf = new Conf();
